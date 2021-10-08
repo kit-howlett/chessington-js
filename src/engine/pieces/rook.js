00f7301da3fe6d16cb08bee0dfb11ee10199d317
Kit Howlett <kit.howlett@gmail.com>
2021-10-08 15:50:29 +0100

    Rook can move laterally

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from "./../square"
+import GameSettings from './../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,25 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const availableMoves = [];
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            const availableMove = Square.at(currentSquare.row, i);
+            
+            if (!availableMove.equals(currentSquare)) {
+                availableMoves.push(availableMove);
+            }
+        }
+        
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            const availableMove = Square.at(i, currentSquare.col);
+            
+            if (!availableMove.equals(currentSquare)) {
+                availableMoves.push(availableMove);
+            }
+        }
+
+        return availableMoves;
     }
 }
