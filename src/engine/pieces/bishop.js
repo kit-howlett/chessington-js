f1f77a6a3579147f3314be0619318fd7ed018a7b
Kit Howlett <kit.howlett@gmail.com>
2021-10-11 12:05:45 +0100

    Bishop can move diagonally.

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from "./../square"
+import GameSettings from './../gameSettings';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,45 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const availableMoves = [];
+        const upperBound = GameSettings.BOARD_SIZE;
+        const lowerBound = -1;
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            const newSquare = Square.at(currentSquare.row + i + 1, currentSquare.col + i + 1);
+            
+            if (newSquare.col < upperBound && newSquare.row < upperBound) {
+                availableMoves.push(newSquare);
+            } 
+        }
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            const newSquare = Square.at(currentSquare.row - i - 1, currentSquare.col - i - 1);
+            
+            if (newSquare.col > lowerBound && newSquare.row > lowerBound) {
+                availableMoves.push(newSquare);
+            } 
+        }
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            const newSquare = Square.at(currentSquare.row + i + 1, currentSquare.col - i - 1);
+            
+            if (newSquare.col > lowerBound && newSquare.row < upperBound) {
+                availableMoves.push(newSquare);
+            } 
+        }
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            const newSquare = Square.at(currentSquare.row - i - 1, currentSquare.col + i + 1);
+            
+            if (newSquare.col < upperBound && newSquare.row > lowerBound) {
+                availableMoves.push(newSquare);
+            } 
+        }
+        
+        return availableMoves;
     }
 }
+
+// GameSettings.BOARD_SIZE
